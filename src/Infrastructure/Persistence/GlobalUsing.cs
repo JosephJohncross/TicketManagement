6757634a34ba2bfa1ee0e4e6892c16445b7d2e8d
JosephJohncross <josephibochi@gmail.com>
2023-04-28 23:54:48 +0100

    feat: services registeration for persistence project in infrastructure

--- src/Infrastructure/Persistence/GlobalUsing.cs
+++ src/Infrastructure/Persistence/GlobalUsing.cs
@@ -1,3 +1,3 @@
 global using Microsoft.EntityFrameworkCore;
-global using TicketManagement.Application;
+global using TicketManagement.Application.Contracts.Persistence;
 global using TicketManagement.Domain.Entities;
---
+++ src/Infrastructure/Persistence/PersistenceServiceRegisteration.cs
@@ -0,0 +1,20 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Configurations.Repositories;
+
+namespace Persistence
+{
+    public static class PersistenceServiceRegisteration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("DefaultConnectionString")));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(BaseRepository<>));
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+
+            return services;
+        }
+    }
+}
